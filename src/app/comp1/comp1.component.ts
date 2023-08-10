f9955552684996c5766d574c1cc0d1cb20e0e773
sajidmtamboli <sajidtamboli1294@gmail.com>
2023-08-10 21:22:01 +0530

    services

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -9,17 +9,21 @@ import { UserService } from '../Services/user.service';
 })
 export class Comp1Component implements OnInit {
   products = {};
+  objUserArray : any[] = [];
   constructor(private objService: Service1Service, private objUser: UserService) { }
 
   ngOnInit() {
-    // const objService = new Service1Service();
+    const objService = new Service1Service();
     this.products = this.objService.products;
-
+ 
     var obj = this.objUser.getUsers();
    
-   obj.subscribe(res=> {
+   obj.subscribe((res: any []) => {
     console.log(res)
+    this.objUserArray = res;
+    
    })
+  
   }
 
 }
